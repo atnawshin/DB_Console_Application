0ce8d8e0b980eb4e7b12683bf1200268d27f1081
atnawshin <atnawshin@gmail.com>
2023-11-04 04:14:27 +0600

    third commit

--- src/DBConsoleApplication.java
+++ src/DBConsoleApplication.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.ArrayDeque;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -11,16 +12,25 @@ public class DBConsoleApplication {
 
 //    Create data or insert data
     private void insertData(){
+        ArrayDeque<String> listOfQuery = new ArrayDeque<>();
         try {
             System.out.println("Connecting to the database...");
             Connection connection = DriverManager.getConnection(DBURL,USERNAME,PASSWORD);
             System.out.println("Connection OK!");
 
-            String query = "insert into student values(105,'Bela bosh','some where in BD','2441139');";
+            String query1 = "insert into student values(106,'Himu','some where in dhaka','938957');";
+
+            String query2 = "insert into student values(107,'Nila','some where in bhutergoli','45768');";
+
+            listOfQuery.add(query1);
+            listOfQuery.add(query2);
 
             Statement statement = connection.createStatement();
 
-            statement.executeUpdate(query);
+            for(String query:listOfQuery){
+                statement.executeUpdate(query);
+            }
+
             System.out.println("Record inserted");
 
             connection.close();
@@ -61,31 +71,57 @@ public class DBConsoleApplication {
     }
 
 //    Update data
-    public void updateData(){
+        private void updateData(){
+        ArrayDeque<String> listOfQuery = new ArrayDeque<>();
         try {
             System.out.println("Connecting to the database...");
             Connection connection = DriverManager.getConnection(DBURL,USERNAME,PASSWORD);
             System.out.println("Connection OK!");
 
-            String query = "update student set name = 'Karim Rahman' where id = 104;";
+            String query1 = "UPDATE student s, parents p set s.phone = '101', p.phone = '102' where s.id = 101 AND p.id = 101;";
+
+            String query2 = "UPDATE student s, parents p set s.phone = '101110', p.phone = '102345' where s.id = 103 AND p.id = 104;";
+
+            listOfQuery.add(query1);
+            listOfQuery.add(query2);
 
             Statement statement = connection.createStatement();
 
-            int resultSet = statement.executeUpdate(query);
-            System.out.println("Data updated");
-
-//            next returns a boolean
-            if(resultSet >0){
-                System.out.println("Student updated successfully");
+            for(String query:listOfQuery){
+                statement.execute(query);
             }
+
+            System.out.println("Record inserted");
 
             connection.close();
 
         } catch (SQLException sqle) {
 //            throw new RuntimeException(sqle);
-            System.out.println(("something went wrong!" + sqle));
+            System.out.println(("something" + sqle));
         }
     }
+
+//    Update data
+//private void updateData(){
+//    try {
+//        System.out.println("Connecting to the database...");
+//        Connection connection = DriverManager.getConnection(DBURL,USERNAME,PASSWORD);
+//        System.out.println("Connection OK!");
+//
+//        String query = "UPDATE student s, parents p set s.phone = '101', p.phone = '102' where s.id = 101 && p.id = 101;";
+//
+//        Statement statement = connection.createStatement();
+//
+//        statement.execute(query);
+//        System.out.println("Record inserted");
+//
+//        connection.close();
+//
+//    } catch (SQLException sqle) {
+////            throw new RuntimeException(sqle);
+//        System.out.println(("something went wrong!" + sqle));
+//    }
+//}
 
 //Delete data
     public void deleteData(){
@@ -115,13 +151,14 @@ public class DBConsoleApplication {
 //    constructor
     public DBConsoleApplication() {
 //        insertData();
-//        retrieveData();
-//          deleteData();
-          updateData();
+        retrieveData();
+//        deleteData();
+//        updateData();
     }
 
     public static void main(String[] args) {
 
         DBConsoleApplication a = new DBConsoleApplication();
+
     }
 }
